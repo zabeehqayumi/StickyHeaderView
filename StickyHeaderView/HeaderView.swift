d43fd5ad285d1ae24521622f9631f393e947bbb9
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-11-22 20:51:17 -0600

    created sticky header demo :

---
+++ StickyHeaderView/HeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderView.swift
+//  StickyHeaderView
+//
+//  Created by Zabeehullah Qayumi on 11/22/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewCell {
+    @IBOutlet weak var lbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
--- StickyHeaderView/ViewController.swift
+++ StickyHeaderView/ViewController.swift
@@ -8,11 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct postSruct {
+    var name : String?
+}
+
+class TableViewController: UITableViewController {
+    
+    var arrOfPost = [postSruct]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        arrOfPost = [postSruct.init(name: "First Header"),
+                     postSruct.init(name: "Second Header"),
+                     postSruct.init(name: "Third Header"),
+                     postSruct.init(name: "Fourth Header"),
+                     postSruct.init(name: "Fifth Header")]
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return arrOfPost.count
+        
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)?.first as! HeaderView
+        
+        headerView.lbl.text = arrOfPost[section].name
+        
+        return headerView
+        
     }
 
 
